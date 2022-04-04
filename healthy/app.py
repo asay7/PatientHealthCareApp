1f853f00900b9c0854d37207a6c77581b32e40b0
asay7 <85703410+asay7@users.noreply.github.com>
2022-04-04 00:40:58 -0400

    Change naming in app.py
    set app to application

--- healthy/app.py
+++ healthy/app.py
@@ -3,30 +3,29 @@ from .routes import homeBP, deviceBP, chatBP, userBP
 import logging
 import pymongo
 
-app = Flask(__name__, instance_relative_config=False)
-app = application
+application = Flask(__name__, instance_relative_config=False)
 logging.basicConfig(filename='app.log', level=logging.DEBUG)
 logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(filename)s: %(lineno)d]')
 
-for handler in app.logger.handlers:
-    app.logger.removeHandler(handler)
+for handler in application.logger.handlers:
+    application.logger.removeHandler(handler)
 
-app.config.from_object('config.DevConfig')
+application.config.from_object('config.DevConfig')
 
 
 '''
-MONGO_URL = app.config["MONGO_URI"]
+MONGO_URL = application.config["MONGO_URI"]
 
 try:
     client = pymongo.MongoClient(host=MONGO_URL, serverSelectionTimeoutMS=2000)
     db = client.server_info()
-    #only register the chat app if the server connection was successful, otherwise display an error
+    #only register the chat application if the server connection was successful, otherwise display an error
 except:
-    app.logger.error("Failed to connect to MongoDb server.")
+    application.logger.error("Failed to connect to MongoDb server.")
 '''
 
-app.register_blueprint(userBP)
-app.register_blueprint(deviceBP)
+application.register_blueprint(userBP)
+application.register_blueprint(deviceBP)
 
 if __name__ == "main":
-    app.run()
+    application.run()
