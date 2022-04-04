ec58a6240b68fc3e0001d3c60a04d716735c957d
asay7 <85703410+asay7@users.noreply.github.com>
2022-04-03 23:52:46 -0400

    update

--- healthy/routes/chat_routes.py
+++ healthy/routes/chat_routes.py
@@ -1,15 +1,21 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, abort
 from flask import current_app as app
-import pymongo
+from werkzeug.exceptions import BadRequest
 
 chatBP = Blueprint(
     'chatBP', __name__
 )
 
+messages_dB = []
+
 
 @chatBP.route('/users/messages/', methods=['POST'])
 def sendMessage():
-
+    try:
+        jsonfile = request.get_json(silent=False, force=False)
+    except BadRequest:
+        abort(400, "Bad jsonfile")
+    messages_dB.append(jsonfile)
     return
 
 
---
+++ healthy/test/test_chat_model.py
@@ -0,0 +1,3 @@
+import pytest
+import json
+from ..models import device_model
